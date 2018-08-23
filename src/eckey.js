46d10ae504391e2e90e44bdfcca992aa0e856916
Jason <jason.shen@camexgames.com>
2018-08-23 18:29:45 +0800

    add eckey

---
+++ src/eckey.js
@@ -0,0 +1,85 @@
+const crypto        = require('crypto');
+const secp256k1     = require('secp256k1');
+
+class ECKey {
+
+  constructor(privKey, compressed) {
+    this._compressed = true;
+    if (typeof compressed === 'boolean')
+      this._compressed = compressed;
+
+    if (privKey)
+      this.privateKey = privKey;
+  }
+
+  get privateKey() {
+    return this.key;
+  }
+
+  set privateKey(bytes) {
+    var byteArr;
+    if (Buffer.isBuffer(bytes)) {
+      this.key = bytes;
+      byteArr = [].slice.call(bytes);
+    } else if (bytes instanceof Uint8Array) {
+      byteArr = [].slice.call(bytes)
+      this.key = Buffer.from(byteArr)
+    } else if (Array.isArray(bytes)) {
+      byteArr = bytes
+      this.key = Buffer.from(byteArr)
+    } else {
+      throw new Error('Invalid type. private key bytes must be either a Buffer, Array, or Uint8Array.')
+    }
+
+    if (bytes.length !== 32) throw new Error('private key bytes must have a length of 32');
+
+    if (this._compressed) {
+      this._exportKey = Buffer.concat([ this.key, Buffer.from([0x01]) ])
+    } else {
+      this._exportKey = Buffer.concat([ this.key ])
+    }
+
+    this._publicKey = null;
+    this._pubKeyHash = null;
+  }
+
+  get privateExportKey() {
+    return this._exportKey;
+  }
+
+  get publicHash() {
+    return this.pubKeyHash;
+  }
+
+  get pubKeyHash() {
+    if (this._pubKeyHash) return this._pubKeyHash;
+    let sha = crypto.createHash('sha256').update(this.publicKey).digest();
+    this._pubKeyHash = crypto.createHash('rmd160').update(sha).digest();
+    return this._pubKeyHash;
+  }
+
+  get publicKey() {
+    if (!this._publicKey)
+      this._publicKey = secp256k1.publicKeyCreate(this.key, this.compressed);
+    return Buffer.from(this._publicKey);
+  }
+
+  get compressed() {
+    return this._compressed;
+  }
+
+  set compressed(val) {
+    let c = !!val;
+    if (c === this._compressed) return;
+
+    let pk = this.privateKey;
+    this._compressed = c;
+    this.privateKey = pk;
+  }
+
+  toString() {
+    return this.privateKey.toString('hex');
+  }
+}
+
+module.exports = ECKey;
---
+++ test/test.js
@@ -0,0 +1,182 @@
+const assert          = require('assert');
+const secureRandom    = require('secure-random');
+const ECKey           = require('../src/eckey.js');
+
+describe('ECKey', function () {
+  describe('+ ECKey()', function () {
+    it('should create a new ECKey', function () {
+      let buf = secureRandom(32, {type: 'Buffer'});
+      let key = new ECKey(buf);
+      assert.equal(key.toString(), buf.toString('hex'));
+      assert.equal(key.compressed, true);
+    });
+  });
+
+  describe('> when new isnt used', function () {
+    it('should create a new ECKey', function () {
+      var bytes = secureRandom(32, {type: 'Buffer'})
+      var buf = new Buffer(bytes)
+      var key = new ECKey(buf)
+      assert.equal(key.toString(), buf.toString('hex'))
+
+      key = new ECKey(buf, true)
+      assert(key.compressed)
+
+      key = new ECKey(buf, false)
+      assert(!key.compressed)
+    })
+  });
+
+  describe('> when input is an Uint8Array', function () {
+    it('should create a new ECKey ', function () {
+      var bytes = secureRandom(32, {type: 'Uint8Array'})
+      assert(bytes instanceof Uint8Array)
+
+      var key = new ECKey(bytes);
+      assert.equal(key.toString(), new Buffer(bytes).toString('hex'))
+      assert.equal(key.compressed, true)
+    })
+  });
+
+  describe('> when input is an Array', function () {
+    it('should create a new ECKey ', function () {
+      var bytes = secureRandom(32, {type: 'Array'})
+      assert(Array.isArray(bytes))
+
+      var key = new ECKey(bytes)
+      assert.equal(key.toString(), new Buffer(bytes).toString('hex'))
+      assert.equal(key.compressed, true)
+    })
+  })
+
+  describe('> when compressed is true', function () {
+    var key = new ECKey(null, true)
+    assert(key.compressed)
+
+    var key2 = new ECKey(secureRandom(32), true)
+    assert(key2.compressed)
+  })
+
+  describe('> when bad data type', function () {
+    it('should throw an error', function () {
+      var data = new Uint16Array(16)
+
+      assert.throws(function () {
+        return new ECKey(data)
+      }, /invalid type/i)
+    })
+  })
+});
+
+
+describe('- compressed', function () {
+  describe('> when false to true', function () {
+    it('should change privateExportKey and all other affected fields', function () {
+      var privateKey = new Buffer('1184cd2cdd640ca42cfc3a091c51d549b2f016d454b2774019c2b2d2e08529fd', 'hex')
+      var key = new ECKey(privateKey, false)
+      assert(!key.compressed)
+      var pubKey = key.publicKey;
+      assert.notStrictEqual(key.privateExportKey.toString('hex').slice(-2), '01', 'ends with 01')
+
+      key.compressed = true;
+
+      assert.notEqual(pubKey.toString('hex'), key.publicKey.toString('hex'))
+      assert.strictEqual(key.privateExportKey.toString('hex').slice(-2), '01', 'ends with 01')
+    })
+  })
+
+  describe('> when true to false', function () {
+    it('should change privateExportKey and all other affected fields', function () {
+      var privateKey = new Buffer('1184cd2cdd640ca42cfc3a091c51d549b2f016d454b2774019c2b2d2e08529fd', 'hex')
+      var key = new ECKey(privateKey, true)
+      assert(key.compressed)
+      var pubKey = key.publicKey
+      assert.strictEqual(key.privateExportKey.toString('hex').slice(-2), '01', 'ends with 01')
+
+      key.compressed = false
+
+      assert.notEqual(pubKey.toString('hex'), key.publicKey.toString('hex'))
+      assert.notStrictEqual(key.privateExportKey.toString('hex').slice(-2), '01', 'ends with 01')
+    })
+  })
+});
+
+describe('- privateKey', function () {
+  it('should return the private key', function () {
+    var privateKeyHex = '1184cd2cdd640ca42cfc3a091c51d549b2f016d454b2774019c2b2d2e08529fd'
+    var key = new ECKey([].slice.call(new Buffer(privateKeyHex, 'hex')))
+    assert.equal(key.privateKey.toString('hex'), privateKeyHex)
+  })
+
+  describe('> when length is not 32', function () {
+    var key = new ECKey()
+    it('should throw an error', function () {
+      assert.throws(function () {
+        key.privateKey = new Buffer('ff33', 'hex')
+      }, /length of 32/i)
+    })
+  })
+});
+
+describe('- privateExportKey', function () {
+  describe('> when not compressed', function () {
+    it('should return the private key', function () {
+      var privateKeyHex = '1184cd2cdd640ca42cfc3a091c51d549b2f016d454b2774019c2b2d2e08529fd'
+      var key = new ECKey(new Buffer(privateKeyHex, 'hex'), false)
+      assert.equal(key.privateExportKey.toString('hex'), privateKeyHex)
+    })
+  })
+
+  describe('> when compressed', function () {
+    it('should return the private key', function () {
+      var privateKeyHex = '1184cd2cdd640ca42cfc3a091c51d549b2f016d454b2774019c2b2d2e08529fd'
+      var key = new ECKey(new Buffer(privateKeyHex, 'hex'), true)
+      assert.equal(key.compressed, true)
+      assert.equal(key.privateExportKey.toString('hex'), privateKeyHex + '01')
+    })
+  })
+})
+
+describe('- publicKey', function () {
+  describe('> when not compressed', function () {
+    it('should return the 65 byte public key', function () {
+      var privateKeyHex = '1184cd2cdd640ca42cfc3a091c51d549b2f016d454b2774019c2b2d2e08529fd'
+      var publicKeyHex = '04d0988bfa799f7d7ef9ab3de97ef481cd0f75d2367ad456607647edde665d6f6fbdd594388756a7beaf73b4822bc22d36e9bda7db82df2b8b623673eefc0b7495'
+      var key = new ECKey([].slice.call(new Buffer(privateKeyHex, 'hex')), false)
+      assert.equal(key.publicKey.length, 65)
+      assert.equal(key.publicKey.toString('hex'), publicKeyHex)
+    })
+  })
+
+  describe('> when compressed', function () {
+    it('should return the 33 byte public key', function () {
+      var privateKeyHex = '1184cd2cdd640ca42cfc3a091c51d549b2f016d454b2774019c2b2d2e08529fd'
+      var publicKeyHex = '03d0988bfa799f7d7ef9ab3de97ef481cd0f75d2367ad456607647edde665d6f6f'
+      var key = new ECKey([].slice.call(new Buffer(privateKeyHex, 'hex')), true)
+
+      assert(key.compressed)
+      assert.equal(key.publicKey.length, 33)
+      assert.equal(key.publicKey.toString('hex'), publicKeyHex)
+    })
+  })
+
+  describe('- publicHash', function () {
+    it('should return the hash 160 of public key', function () {
+      var key = new ECKey(new Buffer('1184cd2cdd640ca42cfc3a091c51d549b2f016d454b2774019c2b2d2e08529fd', 'hex'), false)
+      assert.equal(key.publicHash.toString('hex'), '3c176e659bea0f29a3e9bf7880c112b1b31b4dc8')
+      assert.equal(key.pubKeyHash.toString('hex'), '3c176e659bea0f29a3e9bf7880c112b1b31b4dc8')
+      key.compressed = true
+      assert.equal(key.publicHash.toString('hex'), 'a1c2f92a9dacbd2991c3897724a93f338e44bdc1')
+      assert.equal(key.pubKeyHash.toString('hex'), 'a1c2f92a9dacbd2991c3897724a93f338e44bdc1')
+    })
+  })
+
+  describe('- toString()', function () {
+    it('should show the string representation in...', function () {
+      var privateKeyBytes = [].slice.call(new Buffer('1184CD2CDD640CA42CFC3A091C51D549B2F016D454B2774019C2B2D2E08529FD', 'hex'))
+      var eckey = new ECKey(privateKeyBytes)
+      var s = eckey.toString()
+      assert.equal(s, '1184cd2cdd640ca42cfc3a091c51d549b2f016d454b2774019c2b2d2e08529fd')
+    })
+  })
+});
